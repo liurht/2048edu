fa39944d6f49e2dec4b58930cd14a77e72b7d3e7
liurht <cumtls@gmail.com>
2014-07-05 23:04:59 +0800

    2048 of chinese education

---
+++ js/html_actuator.js
@@ -0,0 +1,218 @@
+﻿function HTMLActuator() {
+  this.tileContainer    = document.querySelector(".tile-container");
+  this.scoreContainer   = document.querySelector(".score-container");
+  this.bestContainer    = document.querySelector(".best-container");
+  this.messageContainer = document.querySelector(".game-message");
+  this.sharingContainer = document.querySelector(".score-sharing");
+
+  this.score = 0;
+}
+
+HTMLActuator.prototype.actuate = function (grid, metadata) {
+  var self = this;
+
+  window.requestAnimationFrame(function () {
+    self.clearContainer(self.tileContainer);
+
+    grid.cells.forEach(function (column) {
+      column.forEach(function (cell) {
+        if (cell) {
+          self.addTile(cell);
+        }
+      });
+    });
+
+    self.updateScore(metadata.score);
+    self.updateBestScore(metadata.bestScore);
+
+    if (metadata.terminated) {
+      if (metadata.over) {
+        self.message(false); // You lose
+      } else if (metadata.won) {
+        self.message(true); // You win!
+      }
+    }
+
+  });
+};
+
+// Continues the game (both restart and keep playing)
+HTMLActuator.prototype.continue = function () {
+  if (typeof ga !== "undefined") {
+    ga("send", "event", "game", "restart");
+  }
+
+  this.clearMessage();
+};
+
+HTMLActuator.prototype.clearContainer = function (container) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+};
+
+HTMLActuator.prototype.addTile = function (tile) {
+  var text=new Array(26);
+  text[0] = " ";
+  text[1] = "一年级";
+  text[2] = "二年级";
+  text[3] = "三年级";
+  text[4] = "四年级";
+  text[5] = "五年级";
+  text[6] = "六年级";
+  text[7] = "初一";
+  text[8] = "初二";
+  text[9] = "初三";
+  text[10] = "高一";
+  text[11] = "高二";
+  text[12] = "高三";
+  text[13] = "大一";
+  text[14] = "大二";
+  text[15] = "大三";
+  text[16] = "大四";
+  text[17] = "研一";
+  text[18] = "研二";
+  text[19] = "研三";
+  text[20] = "博一";
+  text[21] = "博二";
+  text[22] = "博三";
+  text[23] = "博士后";
+  text[24] = " ";
+  text[25] = " ";
+  var self = this;
+  var text2 = function (n) { var r = 0; while (n > 1) r++, n >>= 1; return r; }
+
+  var wrapper   = document.createElement("div");
+  var inner     = document.createElement("div");
+  var position  = tile.previousPosition || { x: tile.x, y: tile.y };
+  var positionClass = this.positionClass(position);
+
+  // We can't use classlist because it somehow glitches when replacing classes
+  var classes = ["tile", "tile-" + tile.value, positionClass];
+
+  if (tile.value > 131072) classes.push("tile-super");
+
+  this.applyClasses(wrapper, classes);
+
+  inner.classList.add("tile-inner");
+  inner.innerHTML = text[text2(tile.value)];
+
+  if (tile.previousPosition) {
+    // Make sure that the tile gets rendered in the previous position first
+    window.requestAnimationFrame(function () {
+      classes[2] = self.positionClass({ x: tile.x, y: tile.y });
+      self.applyClasses(wrapper, classes); // Update the position
+    });
+  } else if (tile.mergedFrom) {
+    classes.push("tile-merged");
+    this.applyClasses(wrapper, classes);
+
+    // Render the tiles that merged
+    tile.mergedFrom.forEach(function (merged) {
+      self.addTile(merged);
+    });
+  } else {
+    classes.push("tile-new");
+    this.applyClasses(wrapper, classes);
+  }
+
+  // Add the inner part of the tile to the wrapper
+  wrapper.appendChild(inner);
+
+  // Put the tile on the board
+  this.tileContainer.appendChild(wrapper);
+};
+
+HTMLActuator.prototype.applyClasses = function (element, classes) {
+  element.setAttribute("class", classes.join(" "));
+};
+
+HTMLActuator.prototype.normalizePosition = function (position) {
+  return { x: position.x + 1, y: position.y + 1 };
+};
+
+HTMLActuator.prototype.positionClass = function (position) {
+  position = this.normalizePosition(position);
+  return "tile-position-" + position.x + "-" + position.y;
+};
+
+HTMLActuator.prototype.updateScore = function (score) {
+  this.clearContainer(this.scoreContainer);
+
+  var difference = score - this.score;
+  this.score = score;
+
+  this.scoreContainer.textContent = this.score;
+
+  if (difference > 0) {
+    var addition = document.createElement("div");
+    addition.classList.add("score-addition");
+    addition.textContent = "+" + difference;
+
+    this.scoreContainer.appendChild(addition);
+  }
+};
+
+HTMLActuator.prototype.updateBestScore = function (bestScore) {
+  this.bestContainer.textContent = bestScore;
+};
+
+HTMLActuator.prototype.message = function (won) {
+  var mytxt=new Array(14);
+  mytxt[0]="小学都没毕业 你妈妈也不答应的";
+  mytxt[1]="小学都没毕业 你爸爸都不能答应";
+  mytxt[2]="亲！加油～";
+  mytxt[3]="亲！加油～";
+  mytxt[4]="亲！加油～";
+  mytxt[5]="亲！加油～";
+  mytxt[6]="亲！加油～";
+  mytxt[7]="亲！加油～";
+  mytxt[8]="亲！加油～";
+  mytxt[9]="亲！加油～";
+  mytxt[10]="亲！加油～";
+  mytxt[11]="亲！加油～";
+  mytxt[12]="亲！加油～";
+  mytxt[13]="亲！加油～";
+  mytxt[14]="亲！加油～";
+  mytxt[15]="亲！加油～";
+  mytxt[16]="亲！加油～";
+  mytxt[17]="亲！加油～";
+
+
+  var text3 = function (m) { var r = 0; while (m > 1) r++, m >>= 1; return r; }
+  var type    = won ? "game-won" : "game-over";
+  var message = won ? "亲！博士后出站了，该好好工作了！" : mytxt[text3(maxscore)-5];
+
+  if (typeof ga !== "undefined") {
+    ga("send", "event", "game", "end", type, this.score);
+  }
+
+  this.messageContainer.classList.add(type);
+  this.messageContainer.getElementsByTagName("p")[0].textContent = message;
+
+  this.clearContainer(this.sharingContainer);
+  this.sharingContainer.appendChild(this.scoreTweetButton());
+  twttr.widgets.load();
+};
+
+HTMLActuator.prototype.clearMessage = function () {
+  // IE only takes one value to remove at a time.
+  this.messageContainer.classList.remove("game-won");
+  this.messageContainer.classList.remove("game-over");
+};
+
+HTMLActuator.prototype.scoreTweetButton = function () {
+  var tweet = document.createElement("a");
+  tweet.classList.add("twitter-share-button");
+  tweet.setAttribute("href", "https://twitter.com/share");
+  tweet.setAttribute("data-via", "oprilzeng");
+  tweet.setAttribute("data-url", "http://oprilzeng.github.io/2048/full");
+  tweet.setAttribute("data-counturl", "http://oprilzeng.github.io/2048/full/");
+  tweet.textContent = "Tweet";
+
+  var text = "I scored " + this.score + " points at PRC2048-Full edition, a game where you " +
+             "join numbers to score high! #PRC2048";
+  tweet.setAttribute("data-text", text);
+
+  return tweet;
+};
